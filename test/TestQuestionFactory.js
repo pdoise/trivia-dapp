6b2780cacf837a4ef3454301bb82ec785cc70190
Phillip Doise <p.doise@gmail.com>
2018-08-04 23:34:33 -0600

    test commentary

--- test/TestQuestionFactory.js
+++ test/TestQuestionFactory.js
@@ -5,6 +5,9 @@ contract('QuestionFactory', function(accounts) {
   var owner = accounts[0];
   var playerOne = accounts[1];
 
+  // Note: Questions are not actually used in the game untill they are approved by the contract owner
+  // Get current length of unapprovedQuestions array to test false positve
+  // Create question struct and make sure unapprovedQuestions has incremented, make sure event fired
   it("should create question", async() => {
     const questionFactory = await QuestionFactory.deployed()
 
@@ -23,6 +26,9 @@ contract('QuestionFactory', function(accounts) {
     assert.equal(eventEmitted, true, "creating question should emit an QuestionCreated event")
   })
 
+  // Note: Once the contract owner approves a question it gets pushed to the games questions array for use
+  // Get current length of unapprovedQuestions array and questions array to test false positve
+  // Approve question as owner and make sure unapprovedQuestions decremented and questions incremented
   it("should approve question", async() => {
     const questionFactory = await QuestionFactory.deployed()
 
@@ -37,6 +43,9 @@ contract('QuestionFactory', function(accounts) {
     assert.equal(questionCount.c[0], initQuestionCount.c[0]+1, "approving a question should increment the questions array")
   })
 
+  // Note: The removeUnapprovedQuestion method serves to remove question in the approveQuestion method(occured above) as well as reject question
+  // Get current length of unapprovedQuestions array to test false positve
+  // Reject question as owner and make sure unapprovedQuestions decremented
   it("should reject question", async() => {
     const questionFactory = await QuestionFactory.deployed()
 
@@ -48,6 +57,7 @@ contract('QuestionFactory', function(accounts) {
     assert.equal(unapprovedQuestionCount.c[0], initUnapprovedQuestionCount.c[0]-1, "rejecting a question should decrease unapproved questions array")
   })
 
+  // Attempt to reject a question as a non admin and assert error
   it("owner should be required to reject question", async() => {
     const questionFactory = await QuestionFactory.deployed()
 
@@ -62,6 +72,7 @@ contract('QuestionFactory', function(accounts) {
     assert.ok(err instanceof Error)
   })
 
+   // Attempt to approve a question as a non admin and assert error
   it("owner should be required to approve question", async() => {
     const questionFactory = await QuestionFactory.deployed()
 
--- test/TestTrivia.js
+++ test/TestTrivia.js
@@ -2,14 +2,16 @@ const Trivia = artifacts.require('./Trivia.sol')
 
 //Due to the nature of the state machine and to not duplicate code, contracts StateMachine.sol,
 //PlayerHelper.sol and Trivia.sol will all be tested in this file. Trivia.sol inherits from all of them.
+
+//This test will run through each stage and transition for one round of trivia and test above contracts accordingly
 contract('Trivia', function(accounts) {
 
-  var owner = accounts[0];
   var playerOne = accounts[1];
   var playerTwo = accounts[2];
 
   const entryFee = web3.toWei(1, "ether")
 
+  //Delay function to mimic time requirements in stages
   function delay(t, v) {
     return new Promise(function(resolve) { 
        setTimeout(resolve.bind(null, v), t)
@@ -18,6 +20,8 @@ contract('Trivia', function(accounts) {
 
   it("should test stage Accepting Entry Fees", async() => {
     const trivia = await Trivia.deployed()
+
+    //get and test init values to test for false positives
     const initPlayerCount = await trivia.getPlayerCount.call()
     const initPot = await trivia.pot.call()
     var playerOneInfo = await trivia.playerInfo.call(playerOne);
@@ -29,14 +33,17 @@ contract('Trivia', function(accounts) {
     assert.equal(playerOneInfo[3], false, "player answered correctly init value should be false")
     assert.equal(playerOneInfo[4], false, "player paid init value should be false")
 
+    //Set up watcher for EntryFeePaid event
     var eventEmitted = false
     var event = trivia.EntryFeePaid()
     await event.watch((err, res) => {
         eventEmitted = true
     })
 
+    //Pay entry fee for player one
     await trivia.payEntryFee({gas: 3000000, from: playerOne, value: entryFee})
 
+    //Assure that players array and pot incremented and event fired
     const newPlayerCount = await trivia.getPlayerCount.call()
     const newPot = await trivia.pot.call()
 
@@ -48,10 +55,13 @@ contract('Trivia', function(accounts) {
   it("should transition to question reveal", async() => {
     const trivia = await Trivia.deployed()
 
+    //Make sure current stage is acceptingEntryFees
     const stage1 = await trivia.stage.call()
     assert.equal(stage1, 0, "stage should default to 0 (accepting fees)")
-
+    
+    //Wait 30 seconds to make sure we fufilled the time requirement of the state machine
     return delay(30000).then( async() => {
+      // Add second player and test that stage transitioned to QuestionReveal
       await trivia.payEntryFee({gas: 3000000, from: playerTwo, value: entryFee})
         
       const stage2 = await trivia.stage.call()
@@ -61,19 +71,26 @@ contract('Trivia', function(accounts) {
 
   it("should test stage Question Reveal", async() => {
     const trivia = await Trivia.deployed()
+
+    //get and test init values to test for false positives
     const initAnswerCount = await trivia.answerCount.call()
     const initWinCount = await trivia.winCount.call()
 
     assert.equal(initAnswerCount.c[0], 0, "Expect answer count to be 0 on init")
     assert.equal(initWinCount.c[0], 0, "Expect win count to be 0 on init")
 
+    //Set up watcher for AnswerSubmitted event
     var eventEmitted = false
     var event = trivia.AnswerSubmitted()
     await event.watch((err, res) => {
         eventEmitted = true
     })
 
+    //Give correct answer for player one
+    //Note: Answer is derived from the question factory for ease of use (not for use in production)
     await trivia.giveAnswer("Shadows", {gas: 3000000, from: playerOne})
+
+    //Assure that answerCount and winCount incremented and event fired
     const newAnswerCount = await trivia.answerCount.call()
 
     assert.equal(newAnswerCount.c[0], 1, "Expect answer count to increment by one when question is answered")
@@ -82,13 +99,18 @@ contract('Trivia', function(accounts) {
 
   it("should transition to question complete", async() => {
     const trivia = await Trivia.deployed()
+
+     //get and test init values to test for false positives
     const initWinCount = await trivia.winCount.call()
     assert.equal(initWinCount.c[0], 0, "Expect win count to be 0 on init")
 
+    //Make sure current stage is QuestionReveal
     const stage2 = await trivia.stage.call()
     assert.equal(stage2, 1, "stage should default to 0 (reveal question)")
 
+    //Wait 20 seconds to make sure we fufilled the time requirement of the state machine
     return delay(20000).then( async() => {
+      // Give incorrect answer for second player and test that stage transitioned to Complete. Win count should not increment from last test :/
       await trivia.giveAnswer("Eating", {gas: 3000000, from: playerTwo})
         
       const stage3 = await trivia.stage.call()
@@ -102,17 +124,20 @@ contract('Trivia', function(accounts) {
   it("should test stage Complete", async() => {
     const trivia = await Trivia.deployed()
 
+    //Set up watcher for AnswerSubmitted event
     var eventEmitted = false
     var event = trivia.PlayerPaid()
     await event.watch((err, res) => {
         eventEmitted = true
     })
-
+    
+    // Pay player one who answered correctly and make sure event fired
     await trivia.payPlayer({gas: 3000000, from: playerOne})
 
     assert.equal(eventEmitted, true, "accepting winnings should emit an PlayerPaid event")
   })
 
+  // Test that both players recieve the expected outcome for the round
   it("should test end game player results", async() => {
     const trivia = await Trivia.deployed()
     var playerOneInfo = await trivia.playerInfo.call(playerOne);
